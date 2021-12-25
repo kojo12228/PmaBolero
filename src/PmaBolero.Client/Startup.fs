9eba8ae216a21db7633175d7c144638514550c39
Kojo Amoasi <kojo12331@hotmail.co.uk>
2021-12-25 19:40:13 +0000

    prevent logging to console in release

--- src/PmaBolero.Client/Startup.fs
+++ src/PmaBolero.Client/Startup.fs
@@ -1,6 +1,7 @@
 namespace PmaBolero.Client
 
 open Microsoft.AspNetCore.Components.WebAssembly.Hosting
+open Microsoft.Extensions.DependencyInjection.Extensions
 open Bolero.Remoting.Client
 
 module Program =
@@ -9,6 +10,12 @@ module Program =
     let Main args =
         let builder = WebAssemblyHostBuilder.CreateDefault(args)
         builder.RootComponents.Add<Pages.Main.MyApp>("#main")
-        builder.Services.AddRemoting(builder.HostEnvironment) |> ignore
+        builder.Services
+            .AddRemoting(builder.HostEnvironment)
+            .Services
+#if RELEASE
+            .RemoveAll<Microsoft.Extensions.Http.IHttpMessageHandlerBuilderFilter>()
+#endif
+            |> ignore
         builder.Build().RunAsync() |> ignore
         0
